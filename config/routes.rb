fdb52c9c5fc5345a4e11e647d43b9d45276958d7
Kelsey Freedman <kfreedma@bates.edu>
2014-05-19 12:21:27 -0400

    monday 1230

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/about' => "page#about"
   get '/search' => "page#search"
   get '/signin' => "page#signin"
+  get '/signup' => "page#signup"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
